c0297ae5e0fbb46af5bce81dd233731540a0edca
Swantan Barua <work.swantan@gmail.com>
2025-10-29 12:47:46 +0530

    Initial Commit

---
+++ Hiya/HiyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiyaApp.swift
+//  Hiya
+//
+//  Created by Swantan Barua on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct HiyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
